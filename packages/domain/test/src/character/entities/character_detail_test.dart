1e14876c14e687bc3d0e41ab2cbef7fe9091190d
Sandro Delgadillo <oscardelgadillodev@outlook.com>
2022-04-30 03:24:26 -0500

    test: character_detail entity tested

---
+++ packages/domain/test/src/character/entities/character_detail_test.dart
@@ -0,0 +1,31 @@
+import 'package:domain/domain.dart';
+import 'package:test/test.dart';
+
+const _detail = CharacterDetail(
+  id: '1',
+  name: 'test',
+  eyeColor: 'Blue',
+  hairColor: 'Blond',
+  skinColor: 'Fair',
+  birthYear: '19BBY',
+  vehicles: [
+    Vehicle(
+      id: '1',
+      name: 'Snowspeeder',
+    ),
+    Vehicle(
+      id: '2',
+      name: 'Imperial Speeder Bike',
+    ),
+  ],
+);
+void main() {
+  test('character_detail => viewer to string', () {
+    expect(
+      '$_detail',
+      'CharacterDetail(id: 1, name: test, '
+          'eyeColor: Blue, hairColor: Blond, '
+          'vehicles: ${_detail.vehicles}, skinColor: Fair, birthYear: 19BBY)',
+    );
+  });
+}
