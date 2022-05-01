0efdfeb23ae91e8e984edcf0ceef2df1a2289b13
Sandro Delgadillo <oscardelgadillodev@outlook.com>
2022-04-30 21:05:53 -0500

    chore: config app repositories

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,22 @@
+import 'package:graphql/client.dart';
+import 'package:graphql_repositories/graphql_repositories.dart';
 import 'package:ravn_challenge/app/app.dart';
 import 'package:ravn_challenge/bootstrap.dart';
+import 'package:ravn_challenge/env/env_production.dart';
 
 void main() {
-  bootstrap(() => const App());
+  final httpLink = HttpLink(
+    environment.baseUrl,
+  );
+  final client = GraphQLClient(
+    cache: GraphQLCache(),
+    link: httpLink,
+  );
+  bootstrap(
+    () => App(
+      characterRepository: GraphQlCharacterRepository(
+        client: client,
+      ),
+    ),
+  );
 }
